4c84983fad3966ee3bac6b7f646392fdc913f3cd
Kirill <kirikNikishin45@gmail.com>
2019-12-10 13:38:46 +0300

    Roating

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { ChildComponent } from './child/child.component';
+import { HomeComponent } from './roating/home/home.component';
+import { NotFoundComponent } from './roating/not-found/not-found.component';
+import { AboutComponent } from './roating/about/about.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    HomeComponent,
+    NotFoundComponent,
+    AboutComponent
   ],
   imports: [
     FormsModule,
---
+++ my-app/src/app/child/child.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent {
+  @Input() userName: string;
+  @Output() userNameChange = new EventEmitter<string>();
+  onNameChange(newName: string) {
+    this.userName = newName;
+    this.userNameChange.emit(newName);
+  }
+  }
