6267c763f10ef0ecca8e8de53955f0fa9b4df5b2
Kirill Nikishin <KirikNikishin45@gmail.com>
2019-12-24 15:51:42 +0300

    2

--- control/src/app/login/login.component.ts
+++ control/src/app/login/login.component.ts
@@ -7,15 +7,33 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   login: string;
   password: string;
+  entered: boolean;
   constructor() { }
 
   ngOnInit() {
     this.login = 'admin1';
     this.password = 'admin1';
+    this.entered= false;
   }
 
   MakeLogin() {
     if (this.login == 'admin' && this.password == 'admin') 
-    {console.log('Красавчик, ты смог войти!')}
+    {
+      console.log('Красавчик, ты смог войти!')
+      this.entered = true
+  }
+  else {
+    const pass = localStorage.getItem(this.password);
+    const log = localStorage.getItem(this.login)
+    if (log === this.login && pass === this.password) {
+      console.log('Красавчик, ты смог войти');
+      this.entered = true;
+    }
+    else {
+      this.entered=false;
+      localStorage.setItem(this.login, this.login);
+      localStorage.setItem(this.password, this.password);
+    }
+  }
   }
 }
