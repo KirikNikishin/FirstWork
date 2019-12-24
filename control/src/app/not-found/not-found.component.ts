9ae7fc1a91b61792e5e86214a0b42b16bd65e243
Kirill Nikishin <KirikNikishin45@gmail.com>
2019-12-24 15:16:41 +0300

    1

--- control/src/app/not-found/not-found.component.ts
+++ control/src/app/not-found/not-found.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found.component.css']
 })
 export class NotFoundComponent implements OnInit {
-
+  CurrentDate=Date.now()
+  Number1 = 3.14
+  Number2 = 2048.1234873472387
+  Number3 = 52369024.824778
+  Number4 = 324789.0443333
+  Message='Ангуляр класс'
   constructor() { }
 
   ngOnInit() {
